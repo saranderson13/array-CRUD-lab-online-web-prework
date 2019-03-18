641421cff404300efad32d46b139c8e41af50f69
Sarah Anderson <saranderson13@gmail.com>
2019-03-18 20:03:16 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,39 @@
 def create_an_empty_array
-  
+  empty_array = []
+  # also could be empty_array = Array.new
 end
 
 def create_an_array
-  
+  lady_xmen = ["storm", "jubilee", "phoenix", "kitty pryde"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
+  # also could be array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
+  # also could be array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
+  # also could be array.last
 end
